6a80c38fbd5aed6b7c88b172e0343bef361a54b0
mdragoutas <mdragoutas@gmail.com>
2026-05-25 20:34:07 +0300

    feat: add Main class to invoke HelloUtil's greeting method

---
+++ src/gr/aueb/cf/java/ch13/Main.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.java.ch13;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Καλούμε την υπηρεσία της Utility Class
+        HelloUtil.sayHello();
+    }
+}
