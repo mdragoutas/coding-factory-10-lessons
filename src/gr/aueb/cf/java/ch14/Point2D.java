36a42db1e04132f2c16a79f34c11fdb8dd1db2ed
mdragoutas <mdragoutas@gmail.com>
2026-05-26 19:35:30 +0300

    feat: add Point2D class extending Point with y-coordinate manipulation

---
+++ src/gr/aueb/cf/java/ch14/Point2D.java
@@ -0,0 +1,36 @@
+package gr.aueb.cf.java.ch14;
+
+public class Point2D extends Point {
+    private double y;
+
+    public Point2D() {
+//        super();
+//        y = 0.0;
+    }
+
+    public Point2D(double x, double y) {
+        super(x);
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        y += 1;
+    }
+
+    @Override
+    protected void movePlusTen() {
+        super.movePlusTen();
+        y += 10;
+    }
+
+}
