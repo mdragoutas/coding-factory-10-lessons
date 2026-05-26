ba98e171bd7d87e54b7a451ea9e8e90907bd1213
mdragoutas <mdragoutas@gmail.com>
2026-05-26 18:45:06 +0300

    feat: add OnlyOneCodingFactoryLazy class with lazy initialization

---
+++ src/gr/aueb/cf/java/ch13/OnlyOneCodingFactoryLazy.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.java.ch13;
+
+/**
+ * Lazy initialization
+ */
+public class OnlyOneCodingFactoryLazy {
+
+    private static OnlyOneCodingFactoryLazy instance;     //null
+
+    private OnlyOneCodingFactoryLazy() {}
+
+    public static OnlyOneCodingFactoryLazy getInstance() {
+        if (instance == null) {
+            instance = new OnlyOneCodingFactoryLazy();
+        }
+        return instance;
+    }
+
+    public void doSomething() {
+        System.out.println("Say Hello");
+    }
+}
