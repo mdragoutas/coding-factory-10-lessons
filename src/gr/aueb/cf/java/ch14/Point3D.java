27601eead0a2f0bbc38a5e2ebde8af204f3a725f
mdragoutas <mdragoutas@gmail.com>
2026-05-26 19:58:53 +0300

    feat: add Point3D class extending Point2D with z-coordinate manipulation

---
+++ src/gr/aueb/cf/java/ch14/Point3D.java
@@ -0,0 +1,34 @@
+package gr.aueb.cf.java.ch14;
+
+public class Point3D extends Point2D {
+    private double z;
+
+    public Point3D() {
+
+    }
+
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        z += 1;
+    }
+
+    @Override
+    protected void movePlusTen() {
+        super.movePlusTen();
+        z += 10;
+    }
+}
