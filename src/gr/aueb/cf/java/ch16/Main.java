313cef2a718bfb6cc6f9ffe4e4523f674d692114
mdragoutas <mdragoutas@gmail.com>
2026-06-02 20:05:16 +0300

    feat: add Book class with Builder pattern for book creation

---
+++ src/gr/aueb/cf/java/ch16/Main.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.java.ch16;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book pythonBook = new Book.Builder(1, "A123").build();
+        Book javaBook = new Book.Builder(2, "B456")
+                .author("Emmanouil")
+                .title("Java Programming")
+                .build();
+    }
+}
