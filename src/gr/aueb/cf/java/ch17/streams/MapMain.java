08b256956bd2b9dba3f228257b530277158998b6
mdragoutas <mdragoutas@gmail.com>
2026-06-09 20:38:21 +0300

    feat(MapMain): add HashMap for country representation and filter cities by name length

--- src/gr/aueb/cf/java/ch17/streams/MapMain.java
+++ src/gr/aueb/cf/java/ch17/streams/MapMain.java
@@ -3,12 +3,20 @@ package gr.aueb.cf.java.ch17.streams;
 import gr.aueb.cf.java.ch17.sorting.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MapMain {
 
     public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("GR", "Greece");
+        map.put("US", "United States of America");
+        map.put("DE", "Germany");
+        map.put("FR", "France");
 
         List<Product> products = new ArrayList<>(List.of(
                 new Product("Apples", 12.7, 40),
@@ -27,6 +35,12 @@ public class MapMain {
                 .collect(Collectors.joining(", "));
 
         System.out.println(listOfDescriptions);
+
+        var filteredCities = map.entrySet().stream()
+                .filter(entry -> entry.getValue().length() <= 10)
+                .collect(Collectors.toSet());
+
+        filteredCities.forEach(System.out::println);
     }
 
 
