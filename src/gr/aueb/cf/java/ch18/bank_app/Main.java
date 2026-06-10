844fc756c896eeba53d900810eb3d7f14d99ccce
mdragoutas <mdragoutas@gmail.com>
2026-06-10 20:04:30 +0300

    feat: add AccountReadOnlyDTO and Main class for bank application

---
+++ src/gr/aueb/cf/java/ch18/bank_app/Main.java
@@ -0,0 +1,53 @@
+package gr.aueb.cf.java.ch18.bank_app;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        String option;
+        String iban;
+        BigDecimal balance;
+
+        while (true) {
+            printMenu();
+            option  = scanner.nextLine().trim();
+
+
+            try {
+                switch (option) {
+                    case "1" -> {
+                        System.out.print("Παρακαλώ εισάγετε το IBAN.");
+                        iban = scanner.next().trim();
+                        System.out.print("Παρακαλώ εισάγετε το αρχικό υπόλοιπο.");
+                        balance = new BigDecimal(scanner.nextLine().trim());
+
+                        // AccountReadOnlyDTO readOnlyDTO = accountController.createNewAccount(iban, balance);
+
+                    }
+                }
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("================================================");
+        System.out.println("|           Υπηρεσία Τράπεζας              |");
+        System.out.println("================================================");
+        System.out.println("\nΥποσύστημα Τράπεζας");
+        System.out.println("        1. Δημιουργία / Ενημέρωση λογαριασμού");
+        System.out.println("        2. Προβολή λογαριασμού");
+        System.out.println("Υποσύστημα Πελατών");
+        System.out.println("        3. Κατάθεση");
+        System.out.println("        4. Ανάληψη");
+        System.out.println("        5. Ερώτηση υπολοίπου");
+        System.out.println("[Qq]. Έξοδος");
+        System.out.print("\nΕισάγετε μία επιλογή:  ");
+    }
+}
---
+++ src/gr/aueb/cf/java/ch18/bank_app/dto/AccountReadOnlyDTO.java
@@ -0,0 +1,40 @@
+package gr.aueb.cf.java.ch18.bank_app.dto;
+
+import java.math.BigDecimal;
+
+public record AccountReadOnlyDTO(String iban, BigDecimal balance) {
+
+    @Override
+    public String toString() {
+        return "IBAN: " + iban + ", Balance: " + balance;
+    }
+}
+
+//public class AccountReadOnlyDTO {
+//    private String iban;
+//    private BigDecimal balance;
+//
+//    public AccountReadOnlyDTO() {}
+//
+//    public AccountReadOnlyDTO(String iban, BigDecimal balance) {
+//        this.iban = iban;
+//        this.balance = balance;
+//    }
+//
+//    public String getIban() {
+//        return iban;
+//    }
+//
+//    public void setIban(String iban) {
+//        this.iban = iban;
+//    }
+//
+//    public BigDecimal getBalance() {
+//        return balance;
+//    }
+//
+//    public void setBalance(BigDecimal balance) {
+//        this.balance = balance;
+//    }
+//
+//}
