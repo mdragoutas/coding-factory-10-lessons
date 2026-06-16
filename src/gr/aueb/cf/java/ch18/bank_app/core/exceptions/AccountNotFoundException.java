58b9f40c19162a9faed5c5bea14df9af7614b399
mdragoutas <mdragoutas@gmail.com>
2026-06-16 18:51:32 +0300

    feat(exceptions): add custom exception classes for account management

---
+++ src/gr/aueb/cf/java/ch18/bank_app/core/exceptions/AccountNotFoundException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.java.ch18.bank_app.core.exceptions;
+
+public class AccountNotFoundException extends Exception {
+
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/gr/aueb/cf/java/ch18/bank_app/core/exceptions/ValidationException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.java.ch18.bank_app.core.exceptions;
+
+public class ValidationException extends Exception {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
