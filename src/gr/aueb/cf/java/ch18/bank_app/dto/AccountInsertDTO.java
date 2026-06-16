0f4e697255df4fe0bc489bf726e8d5b315378abf
mdragoutas <mdragoutas@gmail.com>
2026-06-16 19:06:54 +0300

    feat(dto): add AccountDepositDTO and AccountWithdrawDTO classes; refactor AccountInsertDTO empty method

--- src/gr/aueb/cf/java/ch18/bank_app/dto/AccountInsertDTO.java
+++ src/gr/aueb/cf/java/ch18/bank_app/dto/AccountInsertDTO.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public record AccountInsertDTO(String iban, BigDecimal balance) {
 
-    public static AccountInsertDTO empty(String iban, BigDecimal balance) {
+    public static AccountInsertDTO empty() {
         return new AccountInsertDTO("", BigDecimal.ZERO);
     }
 }
---
+++ src/gr/aueb/cf/java/ch18/bank_app/dto/AccountWithdrawDTO.java
@@ -0,0 +1,6 @@
+package gr.aueb.cf.java.ch18.bank_app.dto;
+
+import java.math.BigDecimal;
+
+public record AccountWithdrawDTO(String iban, BigDecimal balance) {
+}
